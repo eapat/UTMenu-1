1bf5930f7368f60e7471820f88c4fb38faa11d9c
eapat <42146465+eapat@users.noreply.github.com>
2019-07-30 17:02:19 +0500

    Исправление недостатков
    //Убрать ValueFactor
    //Добавить пробелы
    //Стиль_дляМетодовТакойВот
    //привести к int

--- UI/lowLevel/value.c
+++ UI/lowLevel/value.c
@@ -7,26 +7,25 @@
 
 static char valStrBuf[VAL_BUF_SIZE];
 
-//Убрать ValueFactor
-//Добавить пробелы
+
+
 //IncDec
-//Стиль_дляМетодовТакойВот
-//привести к int
-
-void Value_add_units_to_string(Value* this);
-void Value_int_to_string(Value* this);
-void Value_float_to_string(Value* this);
-void Value_bool_to_string(Value* this);
-void Value_enum_to_string(Value* this);
 
 
 
-int Value_Init_As_Int(Value* this, uint16_t* value, char** units, float max, float min, enum ValueFactor factor){
+void Value_addUnitsToString(Value* this);
+void Value_intToString(Value* this);
+void Value_floatToString(Value* this);
+void Value_boolToString(Value* this);
+void Value_enumToString(Value* this);
+
+
+
+int Value_initAsInt(Value* this, int* value, char** units, float max, float min){
 	if (max<min) return -1;
 	this->vl = (void*)value;
 	this->units = units;
 	this->type = VALUE_INT;
-	this->factor = (factor == VALUE_FACT_MILLI)? VALUE_NO_FACT: factor;
 	this->max = roundf(max);
 	this->min = (min <0)? 0 : roundf(min);
 	this->digitsAfterDot = 0;
@@ -34,43 +33,40 @@ int Value_Init_As_Int(Value* this, uint16_t* value, char** units, float max, flo
 
 }
 
-int Value_Init_As_Float(Value* this, float* value, char** units, float max, float min, enum ValueFactor factor, uint8_t dAD){
+int Value_initAsFloat(Value* this, float* value, char** units, float max, float min, uint8_t digitsAfterDot){
 	if (max<min) return -1;
 	this->vl = (void*)value;
 	this->units = units;
 	this->type = VALUE_FLOAT;
-	this->factor = factor;
 	this->max= max;
 	this->min= min;
-	this->digitsAfterDot = dAD;
+	this->digitsAfterDot = digitsAfterDot;
 	return 0;
 }
 
 
-int Value_Init_As_Bool(Value* this, bool* value, char** units){
+int Value_initAsBool(Value* this, bool* value, char** units){
 	this->vl = (void*)value;
 	this->units = units;
 	this->type = VALUE_BOOL;
 	this->max= 1;
 	this->min= 0;
-	this->factor = VALUE_NO_FACT;
 	this->digitsAfterDot = 0;
 	return 0;
 }
 
-int Value_Init_As_Enum(Value* this, uint8_t* value, char** units,  float min){
+int Value_initAsEnum(Value* this, uint8_t* value, char** units,  float min){
 	this->vl = (void*)value;
 	this->units = units;
 	this->type = VALUE_ENUM;
 	this->min = (min <0)? 0 : roundf(min);
 	this->max = sizeof(units) + min;
-	this->factor = VALUE_NO_FACT;
 	this->digitsAfterDot = 0;
 	return 0;
 }
 
 
-char* Value_to_string(Value* this){
+char* Value_toString(Value* this){
 	switch (this->type){
 	case VALUE_INT:
 		Value_int_to_string(this);
@@ -90,17 +86,18 @@ char* Value_to_string(Value* this){
 	return valStrBuf;
 }
 
-void Value_add_units_to_string(Value* this){
+void Value_addUnitsToString(Value* this){
 	int len = strlen(valStrBuf);
-	strncat(valStrBuf, this->units[0], VAL_BUF_SIZE-len-1);
+	strncat(valStrBuf, " ", VAL_BUF_SIZE-len-1);
+	strncat(valStrBuf, this->units[0], VAL_BUF_SIZE-len-2);
 	valStrBuf[VAL_BUF_SIZE-1] = '\0';
 }
 
-void Value_int_to_string(Value* this){
-	sprintf(valStrBuf,"%d",*(uint16_t* )this->vl);
+void Value_intToString(Value* this){
+	sprintf(valStrBuf,"%d",*(int* )this->vl);
 }
 
-void Value_float_to_string(Value* this){
+void Value_floatToString(Value* this){
 	switch (this->digitsAfterDot){
 	case 0:
 		sprintf(valStrBuf,"%.0f",*(float* )this->vl);
@@ -115,14 +112,14 @@ void Value_float_to_string(Value* this){
 }
 
 
-void Value_bool_to_string(Value* this){
+void Value_boolToString(Value* this){
 	uint8_t temp = *(uint8_t* )this->vl;
 	if (temp>1) temp = 1;
 	strncpy(valStrBuf, this->units[temp], VAL_BUF_SIZE-1);
 	valStrBuf[VAL_BUF_SIZE-1] = '\0';
 }
 
-void Value_enum_to_string(Value* this){
+void Value_enumToString(Value* this){
 	uint8_t temp = *(uint8_t* )this->vl;
 
 	if(temp<this->min || temp>=this->max){
@@ -133,5 +130,36 @@ void Value_enum_to_string(Value* this){
 		valStrBuf[VAL_BUF_SIZE-1] = '\0';
 	}
 }
+
+
+void Value_inc(Value* this){
+	switch (this->type){
+	case VALUE_INT:
+
+		break;
+	case VALUE_FLOAT:
+
+		break;
+	case VALUE_BOOL:
+
+		break;
+	case VALUE_ENUM:
+
+		break;
+	}
+}
+
+/*
+void Value_incInt(Value* this){
+	this->
+}
+
+
+void Value_dec(Value* this){
+
+}
+
+
+*/
 
 
