cb348516c0d2f04f9bcb5c010786d25b3620121e
eapat <42146465+eapat@users.noreply.github.com>
2019-07-30 13:05:09 +0500

    работа над Value
    1) Добавлены поля:
     - digitsAfterDot;
     - factor;
    2) Модифицированы методы инициализации

--- UI/lowLevel/value.c
+++ UI/lowLevel/value.c
@@ -7,43 +7,59 @@
 
 
 
-void Value_Init_As_Int(Value* this, uint16_t* value, char** units, float max, float min){
+int Value_Init_As_Int(Value* this, uint16_t* value, char** units, float max, float min, enum ValueFactor factor){
+	if (max<min) return -1;
 	this->vl = (void*)value;
 	this->units = units;
 	this->type = VALUE_INT;
-	this->max= roundf(max);
-	if (min <0)min = 0;
-	this->min= roundf(min);
+	this->factor = (factor == VALUE_FACT_MILLI)? VALUE_NO_FACT: factor;
+	this->max = roundf(max);
+	this->min = (min <0)? 0 : roundf(min);
+	this->digitsAfterDot = 0;
+	return 0;
+
 }
 
-
-void Value_Init_As_Float(Value* this, float* value, char** units, float max, float min){
+int Value_Init_As_Float(Value* this, float* value, char** units, float max, float min, enum ValueFactor factor, uint8_t dAD){
+	if (max<min) return -1;
 	this->vl = (void*)value;
 	this->units = units;
-	this->type = VALUE_INT;
+	this->type = VALUE_FLOAT;
+	this->factor = factor;
 	this->max= max;
 	this->min= min;
+	this->digitsAfterDot = dAD;
+	return 0;
 }
 
 
-void Value_Init_As_Bool(Value* this, bool* value, char** units){
+int Value_Init_As_Bool(Value* this, bool* value, char** units){
 	this->vl = (void*)value;
 	this->units = units;
 	this->type = VALUE_BOOL;
 	this->max= 1;
 	this->min= 0;
+	this->factor = VALUE_NO_FACT;
+	this->digitsAfterDot = 0;
+	return 0;
 }
 
-void Value_Init_As_Enum(Value* this, uint8_t* value, char** units, float max, float min){
+int Value_Init_As_Enum(Value* this, uint8_t* value, char** units, float max, float min){
+	if (max<min) return -1;
 	this->vl = (void*)value;
 	this->units = units;
 	this->type = VALUE_ENUM;
-	if (min <0)min = 0;
-	this->min= roundf(min);
+	this->min = (min <0)? 0 : roundf(min);
 	this->max = sizeof(units) + min;
+	this->factor = VALUE_NO_FACT;
+	this->digitsAfterDot = 0;
+	return 0;
 }
 
 
+char* Value_to_string(void){
+
+}
 
 
 
--- UI/lowLevel/value.h
+++ UI/lowLevel/value.h
@@ -4,6 +4,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+
+#define MAKE_VALUE(Name, Value, Type, Units, max , min, factor, digitsAfterDot) \
+		Value Name = {(void*)&Value, Type, Units, max , min, factor, digitsAfterDot}
+
+
 enum ValueType {
 	VALUE_INT,
 	VALUE_FLOAT,
@@ -11,17 +16,34 @@ enum ValueType {
 	VALUE_ENUM
 };
 
+enum ValueFactor {
+	VALUE_NO_FACT,
+	VALUE_FACT_KILO,
+	VALUE_FACT_MEGA,
+	VALUE_FACT_MILLI,
+	VALUE_FACT_AUTO
+};
 
 typedef struct {
-	void*			vl;
-	enum ValueType	type;
-	char**			units;
-	float			max;
-	float			min;
+	void*				vl;
+	enum ValueType		type;
+	char**				units;
+	float				max;
+	float				min;
+	enum ValueFactor	factor;
+	uint8_t 			digitsAfterDot;	//количество знаков после запятой
 } Value;
 
-#define MAKE_VALUE(Name, Value, Type, Units, max , min) \
-		Value Name = {(void*)&Value, Type, Units, max , min}
+
+
+
+
+int Value_Init_As_Int(Value* this, uint16_t* value, char** units, float max, float min, enum ValueFactor factor);
+int Value_Init_As_Float(Value* this, float* value, char** units, float max, float min, enum ValueFactor factor, uint8_t dAD);
+int Value_Init_As_Bool(Value* this, bool* value, char** units);
+int Value_Init_As_Enum(Value* this, uint8_t* value, char** units, float max, float min);
+
+
 
 
 
