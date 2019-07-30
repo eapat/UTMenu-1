bb11dafa71fe60166ff9d29e07f0ecae1eaa9565
UTProfi <ravil.mashakaev@gmail.com>
2019-07-30 14:46:43 +0500

    Добавлен Canvas_drawFrame
    Исправлены ошибки в выводе рамок и заливки

--- UI/lowLevel/canvas.c
+++ UI/lowLevel/canvas.c
@@ -2,6 +2,41 @@
 #include <string.h>
 #include <stdlib.h>
 
+void Layout_Init(Layout* layout,Canvas* canvas,Layout* sourceLayout){
+
+	uint8_t x=sourceLayout->x;
+	uint8_t y=sourceLayout->y;
+	uint8_t w=sourceLayout->width;
+	uint8_t h=sourceLayout->height;
+
+
+	//Произвольный контейнер
+	if(sourceLayout->type==LAYOUT_CUSTOM){
+		layout->x=x<canvas->width-1?x:0;
+		layout->y=y<canvas->height-1?y:0;
+		layout->width=(layout->x+w<=canvas->width)?w:(canvas->width-x);
+		layout->height=(layout->y+h<=canvas->height)?h:(canvas->height-y);
+	}
+	//Заполнение холста
+	else if(sourceLayout->type==LAYOUT_FILL){
+		layout->x=0;
+		layout->y=0;
+		layout->width=canvas->width;
+		layout->height=canvas->height;
+	}
+	//Центр холста
+	else if(sourceLayout->type==LAYOUT_CENTER){
+		if(w>canvas->width)
+			w=canvas->width;
+		layout->width=w;
+
+		layout->width=w<=canvas->width?w:canvas->width;
+		layout->height=h<=canvas->height?h:canvas->height;
+
+		layout->x=(canvas->width-layout->width)/2;
+		layout->y=(canvas->height-layout->height)/2;
+	}
+}
 
 void Canvas_init(Canvas* canvas,uint8_t width, uint8_t height) {
 	canvas->width = width;
@@ -34,6 +69,61 @@ void Canvas_drawPixel(Canvas* canvas, uint8_t x, uint8_t y, uint8_t color) {
 		if (color) canvas->bitmap[(y / 8)*canvas->width + x] |= 1 << y % 8;
 		else canvas->bitmap[(y / 8)*canvas->width + x] &= ~(1 << y % 8);
 	}
+}
+
+void Canvas_drawLineH(Canvas* canvas, uint8_t x, uint8_t y, uint8_t len) {
+	for (uint8_t w = 0; w < canvas->pen.width; w++) {
+		for (uint8_t i = x; i < x + len; i++) {
+			if (canvas->pen.style == PS_SOLID) Canvas_drawPixel(canvas, i, y + w, canvas->pen.color);
+			if (canvas->pen.style == PS_DOT) {
+				if (i%2 == 0) Canvas_drawPixel(canvas, i, y + w, canvas->pen.color);
+				else Canvas_drawPixel(canvas, i, y + w, !canvas->pen.color);
+			}
+		}
+	}
+}
+
+void Canvas_drawLineV(Canvas* canvas, uint8_t x, uint8_t y, uint8_t len) {
+	for (uint8_t w = 0; w < canvas->pen.width; w++) {
+		for (uint8_t i = y; i < y + len; i++) {
+			if (canvas->pen.style == PS_SOLID) Canvas_drawPixel(canvas, x + w, i, canvas->pen.color);
+			if ((canvas->pen.style == PS_DOT)&(i%2 == 0)) Canvas_drawPixel(canvas, x + w, i, canvas->pen.color);
+		}
+	}
+}
+
+void Canvas_drawFrame(Canvas* canvas, Layout* layout) {
+
+	uint8_t x1=layout->x;
+	uint8_t y1=layout->y;
+	uint8_t x2=layout->x+layout->width-1;
+	uint8_t y2=layout->x+layout->height-1;
+	uint8_t penWidth=canvas->pen.width;
+
+	if ((x1 >= 0)&(x2 < canvas->width)&(y1 >= 0)&(y2 < canvas->height)&(x2 > x1)&(y2 > y1)) {
+			// fill area
+
+			if(canvas->brush.style!=BS_CLEAR)
+			for (uint8_t x = x1+penWidth; x <= x2-penWidth; x++) {
+				for (uint8_t y = y1+penWidth; y <= y2-penWidth; y++) {
+					if(canvas->brush.style == BS_SOLID)
+						Canvas_drawPixel(canvas, x, y, canvas->brush.color);
+					else if(canvas->brush.style == BS_DOT_PTR1)
+						if ((x + y) % 2 == 0) Canvas_drawPixel(canvas, x, y, canvas->pen.color);
+						else Canvas_drawPixel(canvas, x, y, !canvas->pen.color);
+					else if(canvas->brush.style == BS_DOT_PTR2)
+						if (((x - x1) % 2 == 0) & ((y - y1) % 2 == 0)) Canvas_drawPixel(canvas, x, y, canvas->pen.color);
+						else Canvas_drawPixel(canvas, x, y, !canvas->pen.color);
+				}
+			}
+
+
+			// draw borders
+			Canvas_drawLineH(canvas, x1, y1, x2 - x1);
+			Canvas_drawLineH(canvas, x1, y2-penWidth, x2 - x1);
+			Canvas_drawLineV(canvas, x1, y1, y2 - y1);
+			Canvas_drawLineV(canvas, x2-penWidth, y1, y2 - y1);
+		}
 }
 
 
@@ -92,4 +182,5 @@ void Canvas_drawString(Canvas* canvas, uint8_t x, uint8_t y, char *s,Font* font)
 		s++;
 	}
 }
+
 
--- UI/ui.c
+++ UI/ui.c
@@ -27,26 +27,60 @@ void UI_init(void){
 	Boolpin_init(&btnFunc,BTN_FUNC_PORT,BTN_FUNC_PIN,BTN_DEBOUNCE_MS,0,0);
 
 	Font_init(&fontRegular,FONT_5x8, F_SPACING_0, FS_NORMAL, BG_TRANS);
-	Font_init(&fontBold,FONT_8x12B, F_SPACING_0, FS_INVERT, BG_TRANS);
+	Font_init(&fontBold,FONT_8x12B, F_SPACING_0, FS_NORMAL, BG_FILL);
 
 	Canvas_init(&canvas,128,64);
+	canvas.pen.color=1;
+
+	canvas.pen.style=PS_DOT;
+
+	canvas.brush.color=1;
+
+
+
 	LCD_init();
 
 	Layout layout={LAYOUT_FILL,0,0,70,40};
 
 	MenuWindow_init(&menuWindow,&canvas,&fontBold,layout);
 	MenuWindow_setMenuList(&menuWindow,&rootItem);
-
-
 }
 
 //Обработчик пользователского интерфейса, обновление дисплея, кнопок
 void UI_handler(void){
+	static uint8_t i=0;
+	static bool up=true;
 	Canvas_clear(&canvas);
+
+	Layout layout={LAYOUT_FILL,0,0,128,64};
+
+	canvas.pen.width=i;
+
+	if(Boolpin_update(&btnPrev))
+		{
+			i--;
+		}
+
+	if(Boolpin_update(&btnNext)){
+			i++;
+		}
+
+	if(Boolpin_update(&btnFunc))
+	{
+		canvas.brush.style++;
+		if(canvas.brush.style>3)
+			canvas.brush.style=0;
+	}
+
+	Canvas_drawFrame(&canvas,&layout);
+	Canvas_drawString(&canvas,10,10,"Текст",&fontBold);
+	//Canvas_drawLineV(&canvas,0,0,40);
+
+	/*
 
 	if(Boolpin_update(&btnPrev))
 	{
-			MenuWindow_decPosition(&menuWindow);
+		MenuWindow_decPosition(&menuWindow);
 	}
 	if(Boolpin_update(&btnNext)){
 		MenuWindow_incPosition(&menuWindow);
@@ -58,7 +92,7 @@ void UI_handler(void){
 			MenuWindow_back(&menuWindow);
 
 	MenuWindow_draw(&menuWindow);
+	*/
 
 	LCD_draw(&canvas);
-
 }
