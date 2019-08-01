828d6039147fcd17652ba7c14fc22af0b16a754a
eapat <42146465+eapat@users.noreply.github.com>
2019-08-01 14:53:45 +0500

    Edit_window
    1) Параметры заголовка и тела окна сгруппированы в соответствующие EWArea;
    2) Добавлена функция бегущей строки к заголовку;
    3) Реаолизована функция isRuning

--- UI/windows/edit_window.c
+++ UI/windows/edit_window.c
@@ -1,31 +1,32 @@
 #include <stdbool.h>
 #include "edit_window.h"
 #include "value.h"
-
+#define EW_SHIFT_TIME 300
+#define EW_SHIFT_PAUSE 1000
 
 void EditWindow_init(EditWindow* this,Canvas* canvas,Font* headerFont, Font* bodyFont, Layout layout){
 	this->canvas = canvas;
-	this->headerFont = headerFont;
-	this->bodyFont = bodyFont;
+	this->header.font = headerFont;
+	this->body.font = bodyFont;
 	this->mainLayout = layout;
 	this->inProgress = false;
 
-	this->headerLayout.height = headerFont->height + headerFont->height/2;
-	this->headerLayout.width = this->mainLayout.width;
-	this->headerLayout.x = this->mainLayout.x;
-	this->headerLayout.y = this->mainLayout.y;
-	this->headerStyle = (headerFont->inversion == FS_INVERT)? FRAME_WHITE: FRAME_BLACK;
-	if ( this->mainLayout.height < (this->headerLayout.height + bodyFont->height + bodyFont->height/2) ){
+	this->header.layout.height = headerFont->height + headerFont->height/2;
+	this->header.layout.width = this->mainLayout.width;
+	this->header.layout.x = this->mainLayout.x;
+	this->header.layout.y = this->mainLayout.y;
+	this->header.style = (headerFont->inversion == FS_INVERT)? FRAME_WHITE: FRAME_BLACK;
+	if ( this->mainLayout.height < (this->header.layout.height + bodyFont->height + bodyFont->height/2) ){
 	 while(1){
 		 //Ошибка инициализации - недостаточная высота Layout при заданных шрифтах
 	 }
 	}
 
-	this->bodyLayout.height = this->mainLayout.height - this->headerLayout.height;
-	this->bodyLayout.width = this->mainLayout.width;
-	this->bodyLayout.x = this->mainLayout.x;
-	this->bodyLayout.y = this->mainLayout.y + this->headerLayout.height;
-	this->bodyStyle = (bodyFont->inversion == FS_INVERT)? FRAME_WHITE: FRAME_BLACK;
+	this->body.layout.height = this->mainLayout.height - this->header.layout.height;
+	this->body.layout.width = this->mainLayout.width;
+	this->body.layout.x = this->mainLayout.x;
+	this->body.layout.y = this->mainLayout.y + this->header.layout.height;
+	this->body.style = (bodyFont->inversion == FS_INVERT)? FRAME_WHITE: FRAME_BLACK;
 
 }
 
@@ -37,13 +38,18 @@ void EditWindow_start(EditWindow* this, Value* val, char* header){
 }
 
 
-void EditWindow_draw(EditWindow* this){
+void EditWindow_draw(EditWindow* this, uint32_t currentTime){
 	if (this->inProgress){
+		int delay = !this->shStr.shiftFlag? EW_SHIFT_PAUSE : EW_SHIFT_TIME;
+		if ((currentTime-this->shStr.prevTime) > delay){
+			this->shStr.prevTime=currentTime;
+			this->shStr.shift=!this->shStr.shiftFlag?this->shStr.shiftFlag+1:0;
+		}
 		Canvas_drawFrame(this->canvas, &this->mainLayout, FRAME_WHITE);
-		Canvas_drawFrame(this->canvas, &this->bodyLayout, this->bodyStyle);
-		Canvas_drawFrame(this->canvas, &this->headerLayout, this->headerStyle);
-		Canvas_drawAlignedString(this->canvas,&this->headerLayout,this->headerFont,this->headerText,ALIGN_CENTER,0);
-		Canvas_drawAlignedString(this->canvas,&this->bodyLayout,this->bodyFont,Value_toString(&this->vlCopy),ALIGN_CENTER,0);
+		Canvas_drawFrame(this->canvas, &this->body.layout, this->body.style);
+		Canvas_drawFrame(this->canvas, &this->header.layout, this->header.style);
+		this->shStr.shiftFlag = Canvas_drawAlignedString(this->canvas,&this->header.layout,this->header.font,this->headerText,ALIGN_CENTER,this->shStr.shift);
+		Canvas_drawAlignedString(this->canvas,&this->body.layout,this->body.font,Value_toString(&this->vlCopy),ALIGN_CENTER,0);
 	}
 
 }
@@ -57,17 +63,24 @@ void EditWindow_inc(EditWindow* this){
 		Value_inc(this);
 	}
 }
+
 void EditWindow_dec(EditWindow* this){
 	if (this->inProgress){
 		Value_dec(this);
 	}
 }
+
 void EditWindow_enter(EditWindow* this){
 	if (this->inProgress){
 		Value_copy(&this->vlCopy,this->vlPt);
 		EditWindow_stop(this);
 	}
 }
+
 void EditWindow_back(EditWindow* this){
 	EditWindow_stop(this);
+}
+
+bool EditWindow_isRuning(EditWindow* this){
+	return this->inProgress;
 }
--- UI/windows/edit_window.h
+++ UI/windows/edit_window.h
@@ -6,28 +6,38 @@
 #include "value.h"
 
 typedef struct {
+	uint32_t prevTime;
+	uint8_t shiftFlag;
+	uint8_t shift;
+}EWShiftString;
+
+typedef struct {
+	Font* font;
+	Layout layout;
+	enum Frame_style style;
+}EWArea;
+
+
+typedef struct {
 	Canvas* canvas;
-	Font* headerFont;
-	Font* bodyFont;
 	Layout mainLayout;
-	Layout headerLayout;
-	Layout bodyLayout;
-	enum Frame_style headerStyle;
-	enum Frame_style bodyStyle;
+	EWArea header;
+	EWArea body;
 	bool inProgress;
 	Value* vlPt;
 	Value vlCopy;
 	char* headerText;
+	EWShiftString shStr;
 }EditWindow;
 
 void EditWindow_init(EditWindow* this,Canvas* canvas,Font* headerFont, Font* bodyFont, Layout layout);
 void EditWindow_start(EditWindow* this, Value* val, char* header);
 void EditWindow_stop(EditWindow* this);
-void EditWindow_draw(EditWindow* this);
+void EditWindow_draw(EditWindow* this, uint32_t currentTime);
 void EditWindow_inc(EditWindow* this);
 void EditWindow_dec(EditWindow* this);
 void EditWindow_enter(EditWindow* this);
 void EditWindow_back(EditWindow* this);
-
+bool EditWindow_isRuning(EditWindow* this);
 
 #endif
